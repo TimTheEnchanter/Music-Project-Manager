13aed52d01b1a9a32d7368a57503c36208813c7d
James Parrott <japarrott@shaw.ca>
2024-10-18 17:39:50 -0600

    Add controller with create and read function

---
+++ MusicProjectManager/Controllers/MusicProjectManagerController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicProjectManager.Controllers
+{
+    public class MusicProjectManagerController : Controller
+    {
+        // To create View of this Action result
+        public ActionResult create()
+        {
+            return View();
+        }
+
+        // Specify the type of attribute i.e.
+        // it will add the record to the database
+        [HttpPost]
+        public ActionResult create(Project model)
+        {
+
+            // To open a connection to the database
+            using (var context = new musicCRUDEntities())
+            {
+                // Add data to the particular table
+                context.Project.Add(model);
+
+                // save the changes
+                context.SaveChanges();
+            }
+            string message = "Created the record successfully";
+
+            // To display the message on the screen
+            // after the record is created successfully
+            ViewBag.Message = message;
+
+            // write @Viewbag.Message in the created
+            // view at the place where you want to
+            // display the message
+            return View();
+        }
+
+        [HttpGet] // Set the attribute to Read
+        public ActionResult Read()
+        {
+            using (var context = new musicCRUDEntities())
+            {
+                var data = context.Project.ToList(); // Return the list of data from the database
+                return View(data);
+            }
+
+        }
+    }
