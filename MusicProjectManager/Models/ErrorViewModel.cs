29a4b1d5c4aff3adb7c7d80fdb1f05b40de1ef20
James Parrott <japarrott@shaw.ca>
2024-10-20 14:50:53 -0600

    Updated project, added model, view, new controller
    Getting a handle on all of the environment changes for 2022 that Microsoft is doing is interesting to say the least

--- MusicProjectManager/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MusicProjectManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
